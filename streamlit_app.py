f90bc5a3802f3d3a8293af957d220f5dc2c4dd7f
UmmulwaraGit <wara_2050@yahoo.com>
2024-06-05 13:03:51 +0500

    Add API integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -38,3 +39,7 @@ if ingrediant_list:
     if time_to_submit:
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered! "+name_txt +" :cup_with_straw:", icon="✅" )
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
